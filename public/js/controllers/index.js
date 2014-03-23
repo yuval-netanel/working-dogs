f34a9c0515d4c5266c839cd2de62775a5f698aa6
Yuval Netanel <yuval.netanel@klarna.com>
2014-03-23 18:30:50 +0200

    calendar selection

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,24 +1,41 @@
 angular.module('mean.system').controller('IndexController', ['$scope', 'Global', function ($scope, Global) {
     $scope.global = Global;
 
+    $scope.alertEventOnClick = function(date,allDay, _, view){
+      var title = prompt('Event Title:');
+        if (title) {
+          $scope.myCalendar.fullCalendar('renderEvent', {
+                                                             title: title,
+                                                             date: date,
+                                                             allDay: allDay
+                                                     },
+                                                     true // make the event "stick"
+                                             );
+                                     }
+      $scope.myCalendar.fullCalendar( 'unselect' );
+    };
+
     /* config object */
     $scope.uiConfig = {
       calendar:{
         height: 600,
         editable: true,
+        selectable: true,
+        selectHelper: true,
         header:{
           left: '',
           center: 'title',
           right: 'today prev,next'
         },
-        dayClick: $scope.alertEventOnClick,
+        select: $scope.alertEventOnClick,
         eventDrop: $scope.alertOnDrop,
         eventResize: $scope.alertOnResize
       }
     };
 
-    $scope.alertEventOnClick = function(){
-      return true;
-    };
+    $scope.eventSources = {};
+
+
+
 
 }]);
