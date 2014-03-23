d847079372235012778c167d155bd77e5630ae95
Elad Maimon <elad.maimon@klarna.com>
2014-03-23 18:23:38 +0200

    fix md5 dependency

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.articles', 'mean.dogs', 'angularjs-gravatardirective', 'md5', 'ui.calendar']);
+angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.articles', 'mean.dogs', 'angularjs-gravatardirective', 'ui.calendar']);
 
 angular.module('mean.system', []);
 angular.module('mean.articles', []);
